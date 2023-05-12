ef5c40ee63f2260ce11a7edd51af814a67f97163
ASUS Desktop <1033718596@qq.com>
2023-05-11 21:02:38 -0700

    fix bugs

--- run_experiments.py
+++ run_experiments.py
@@ -5,8 +5,11 @@ from experiments import translation
 
 @hydra.main(config_name="configs/config")
 def main(config: omegaconf.DictConfig):
+    print(config)
+    return
     if config.experiment_type == "translation":
         translation.main(config=config.translation)
+    
 
 
 if __name__ == "__main__":
